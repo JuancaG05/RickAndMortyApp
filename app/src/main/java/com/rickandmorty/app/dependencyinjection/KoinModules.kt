f5c15f7e8e5f611af80bc6e08baf015946428ccd
Juan Carlos Garrote <juanca_arrabal@hotmail.com>
2025-04-05 21:01:08 +0200

    feat: added ViewModel for characters

--- app/src/main/java/com/rickandmorty/app/dependencyinjection/KoinModules.kt
+++ app/src/main/java/com/rickandmorty/app/dependencyinjection/KoinModules.kt
@@ -5,12 +5,14 @@ import com.rickandmorty.app.data.characters.datasources.ICharactersRemoteDataSou
 import com.rickandmorty.app.data.characters.repository.CharactersRepository
 import com.rickandmorty.app.domain.characters.repository.ICharactersRepository
 import com.rickandmorty.app.domain.characters.usecases.GetCharactersUseCase
+import com.rickandmorty.app.presentation.characters.CharactersViewModel
+import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val presentationModule = module {
-
+    viewModelOf(::CharactersViewModel)
 }
 
 val dataModule = module {
---
+++ app/src/main/java/com/rickandmorty/app/presentation/characters/CharactersViewModel.kt
@@ -0,0 +1,29 @@
+package com.rickandmorty.app.presentation.characters
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.rickandmorty.app.domain.characters.Character
+import com.rickandmorty.app.domain.characters.usecases.GetCharactersUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class CharactersViewModel(
+    private val getCharactersUseCase: GetCharactersUseCase,
+) : ViewModel() {
+
+    private val _listOfCharacters: MutableStateFlow<Pair<List<Character?>, Int?>> = MutableStateFlow(Pair(emptyList(), null))
+    val listOfCharacters: StateFlow<Pair<List<Character?>, Int?>> = _listOfCharacters
+
+    init {
+        getCharacters(page = 1)
+    }
+
+    fun getCharacters(page: Int) {
+        viewModelScope.launch {
+            val result = getCharactersUseCase.run(page)
+            _listOfCharacters.update { result }
+        }
+    }
+}
