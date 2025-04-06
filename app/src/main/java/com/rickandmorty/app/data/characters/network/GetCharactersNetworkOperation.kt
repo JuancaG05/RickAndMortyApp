0df3a8e87efb0583bcc20ec849e46dde45e8c7c0
Juan Carlos Garrote <juanca_arrabal@hotmail.com>
2025-04-06 22:51:34 +0200

    feat: avoid the app from crashing when there is no Internet connection

--- app/src/main/java/com/rickandmorty/app/data/characters/network/GetCharactersNetworkOperation.kt
+++ app/src/main/java/com/rickandmorty/app/data/characters/network/GetCharactersNetworkOperation.kt
@@ -1,5 +1,6 @@
 package com.rickandmorty.app.data.characters.network
 
+import android.util.Log
 import com.apollographql.apollo3.api.Optional
 import com.rickandmorty.app.GetCharactersPageQuery
 import com.rickandmorty.app.data.ApolloClient
@@ -8,9 +9,19 @@ import com.rickandmorty.app.domain.characters.CharacterGender
 class GetCharactersNetworkOperation {
 
     suspend fun run(page: Int, filterByGender: CharacterGender?): GetCharactersPageQuery.Characters? {
-        val response = ApolloClient.client.query(
-            GetCharactersPageQuery(Optional.presentIfNotNull(page), Optional.presentIfNotNull(filterByGender?.name))
-        ).execute()
-        return response.data?.characters
+        try {
+            val response = ApolloClient.client.query(
+                GetCharactersPageQuery(
+                    Optional.presentIfNotNull(page),
+                    Optional.presentIfNotNull(filterByGender?.name)
+                )
+            ).execute()
+            return response.data?.characters
+        }
+        catch (e: Exception) {
+            Log.e("GetCharactersNetworkOperation","Network operation to retrieve characters could not be executed: ${e.message.toString()}")
+            return null
+        }
+
     }
 }
