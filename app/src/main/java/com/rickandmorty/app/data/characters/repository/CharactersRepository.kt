cabf8e2b8f09aae68f930d2e3adbc67732d7b69e
Juan Carlos Garrote <juanca_arrabal@hotmail.com>
2025-04-07 08:35:24 +0200

    feat: respect favourite characters when accessing from deep link

--- app/src/main/java/com/rickandmorty/app/data/characters/repository/CharactersRepository.kt
+++ app/src/main/java/com/rickandmorty/app/data/characters/repository/CharactersRepository.kt
@@ -14,8 +14,16 @@ class CharactersRepository(
     override suspend fun getCharacters(page: Int, filterByGender: CharacterGender?): Pair<List<Character?>, Int?> =
         charactersRemoteDataSource.getCharacters(page, filterByGender)
 
-    override suspend fun getCharacter(id: String): Character? =
-        charactersRemoteDataSource.getCharacter(id)
+    override suspend fun getCharacter(id: String): Character? {
+        val character = charactersRemoteDataSource.getCharacter(id)
+        character?.let {
+            charactersLocalDataSource.getFavouriteCharacters().find { it.id == id }?.let {
+                character.isFavourite = true
+            }
+        }
+        return character
+    }
+
 
     override suspend fun upsertFavouriteCharacter(character: Character) {
         charactersLocalDataSource.upsertFavouriteCharacter(character)
